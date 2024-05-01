331b3d63726c9285602868514cefbfff11ea84b0
ice6504 <aphinan6504@gmail.com>
2024-05-01 13:25:52 +0700

    Set up project

--- src/app/page.js
+++ src/app/page.js
@@ -49,7 +49,7 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+              <i class="fa-solid fa-arrow-right"></i>
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
