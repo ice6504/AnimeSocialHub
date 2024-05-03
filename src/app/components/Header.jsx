c34f40c9effb309ff7751aebc08df381ade610cd
ice6504 <aphinan6504@gmail.com>
2024-05-03 16:00:15 +0700

    responsive navbar

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -7,19 +7,27 @@ function Header() {
         <div className="navbar-start space-x-2">
           <Link
             href="/"
-            className="text-primary font-semibold text-2xl text-center transition-all ease-in-out duration-300 px-4 active:scale-95"
+            className="max-sm:hidden text-primary font-semibold text-2xl text-center transition-all ease-in-out duration-300 px-4 active:scale-95"
           >
             Anihub
           </Link>
           {/* search */}
-          <label className="input input-sm h-10 w-28 focus-within:w-64 hover:w-64 input-ghost bg-white text-secondary rounded-full flex items-center gap-2 transition-all ease-in-out duration-500">
-            <i class="fa-solid fa-magnifying-glass font-bold"></i> 
+          <label className="input input-sm h-10 w-28 sm:w-40 focus-within:sm:w-64 hover:sm:w-64 text-secondary rounded-full flex items-center gap-2 transition-all ease-in-out duration-500">
+            <i className="fa-solid fa-magnifying-glass font-bold"></i>
             <input type="text" className="grow" placeholder="Search" />
           </label>
         </div>
+        <div className="navbar-center sm:hidden">
+          <Link
+            href="/"
+            className="text-primary font-semibold text-2xl text-center transition-all ease-in-out duration-300 px-4 active:scale-95"
+          >
+            Anihub
+          </Link>
+        </div>
         <div className="navbar-end">
           <Link href="" className="btn btn-circle btn-ghost">
-            <i class="fa-solid fa-comment-dots fa-2xl"></i>
+            <i className="fa-solid fa-comment-dots fa-2xl"></i>
           </Link>
         </div>
       </div>
